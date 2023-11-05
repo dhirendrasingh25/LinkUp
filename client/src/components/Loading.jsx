aa42b825f17ba073dc37e382b99250a64bd9d0e3
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-11-05 12:57:04 +0530

    added Login Page

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+const Loading = () => {
+    return (
+      <div className='dots-container'>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+      </div>
+    );
+  };
+  
+  export default Loading;
+  
