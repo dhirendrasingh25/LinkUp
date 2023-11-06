b3b7a0277ace21ad85e551d595652cdf350bdd79
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-11-06 14:50:49 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -4,12 +4,18 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import express from "express";
-
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8008;
+
+async function dbConnection() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("Database Connected");
+}
+dbConnection().catch((err) => console.log(err));
 
 app.use(cors());
 app.use(helmet());
